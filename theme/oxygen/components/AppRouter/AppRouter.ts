7c46dee02fde1c8fc51d35716e851562056562b2
Kenjie Terrado <terradoflex@gmail.com>
2024-08-25 00:25:05 +0800

    rmv yotpo js

--- theme/oxygen/components/AppRouter/AppRouter.ts
+++ theme/oxygen/components/AppRouter/AppRouter.ts
@@ -100,13 +100,6 @@ app.component<AppRouter>('AppRouter',(
                     location.href = '/login.html?app_key='+appKey
                     return
                 }
-                (function e(){
-                    var e=document.createElement("script");
-                    e.type="text/javascript",
-                    e.async=true,
-                    e.src="//staticw2.yotpo.com/"+appKey+"/widget.js";
-                    var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}
-                )();
             }
             await ActivatePage()
         },3000)
