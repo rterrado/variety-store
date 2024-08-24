04dd772239f8d032333427a66a49969ccc98292f
Kenjie Terrado <terradoflex@gmail.com>
2024-08-24 23:25:47 +0800

    error section

--- theme/oxygen/components/AppRouter/AppRouter.php
+++ theme/oxygen/components/AppRouter/AppRouter.php
@@ -6,7 +6,33 @@
     <div plunc-if="state=='active'">
         <?php template_content(); ?>
     </div>
-    <div plunc-if="state=='error'">
+    <div plunc-if="state=='error'" class="width-24 device-height-24 display-flex align-items-center justify-content-center">
         <!-- Apply error page here -->
+        <div class="concrete-width-large-3@desktop width-24@mobile padding-x-13@mobile">
+            <div class="display-flex align-items-center">
+                <div class="text-24 color-gray-scale-super-dark">
+                    {{error.code}} /
+                </div>
+                <div class="text-24 font-weight-800 margin-left-5 color-gray-scale-super-dark">
+                    <span plunc-if="error.code==400">Bad Request</span>
+                    <span plunc-if="error.code==401">Access Denied</span>
+                    <span plunc-if="error.code==404">Not Found</span>
+                    <span plunc-if="error.code==500">Internal Server Error</span>
+                </div>
+            </div>
+            <div class="margin-top-8 text-4 line-height-17 font-weight-400">
+                {{error.message}} If you think this is incorrect, please contact me below.
+            </div>
+            <div class="margin-top-13 display-flex align-items-center">
+                <?php snippet('Icons/SVG',[
+                    'path' => '/Paths/email.svg',
+                    'class' => 'concrete-width-extra-small-13 color-primary svg-fill-none',
+                    'icon_stroke_width' => '1.5'
+                ]); ?>
+                <div class="text-4 font-weight-500 margin-left-7">
+                    rterrado@yotpo.com
+                </div>
+            </div>
+        </div>
     </div>
 </template>
