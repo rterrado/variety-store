c4def9fdf7e84e65236648e598dbbb130e0c26e8
Kenjie Terrado <terradoflex@gmail.com>
2024-09-26 23:53:05 +0800

    adding favicons

--- theme/oxygen/index.php
+++ theme/oxygen/index.php
@@ -5,7 +5,10 @@ use Kenjiefx\ScratchPHP\App\Build\BuildHelpers;
 <!DOCTYPE html>
 <html lang="en">
     <head>
-    <meta content='width=device-width, initial-scale=1' name='viewport'/>
+        <meta content='width=device-width, initial-scale=1' name='viewport'/>
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png">
         <title><?php echo page_title(); ?></title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
